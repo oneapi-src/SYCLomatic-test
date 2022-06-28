820e51e5d323f4c84b61f6926740e2c7a2c75f60
0x12CC <68250218+0x12CC@users.noreply.github.com>
2022-06-27 20:19:53 -0400

    Fix math API test issues (#45)
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>

--- features/feature_case/math/math-exec.cu
+++ features/feature_case/math/math-exec.cu
@@ -18,13 +18,13 @@ __global__ void testMathFunctions(char *const TestResults) {
     __half2 h2 = __halves2half2(1.1, 1.1);
     h2 = __hadd2(h2, h2);
     TestResults[0] =
-        (__low2half(h2) == half(2.2) && __high2half(h2) == half(2.2));
+        (__low2half(h2) == __half(2.2) && __high2half(h2) == __half(2.2));
   }
 
   {
-    __half h = half(0.5);
+    __half h = __half(0.5);
     h = hrcp(h);
-    TestResults[1] = (h == half(2));
+    TestResults[1] = (h == __half(2));
   }
 
   {
@@ -49,7 +49,8 @@ int main() {
   for (int i = 0; i < NumberOfTests; i++) {
     if (TestResults[i] == 0) {
       std::cout << "Test " << i << " failed" << std::endl;
+      return 1;
     }
-    assert(TestResults[i] != 0);
   }
+  return 0;
 }
