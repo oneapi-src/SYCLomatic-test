92beaecd9987d6c122e20d4f4164df558ff58dee
Huang, Andy <andy.huang@intel.com>
2022-07-19 10:30:48 +0800

    Delete negative_test.cfg and use do_test.py instead
    Signed-off-by: Huang, Andy <andy.huang@intel.com>

---
+++ behavior_tests/src/ngt-UDR-path/do_test.py
@@ -0,0 +1,28 @@
+# ====------ do_test.py---------- *- Python -* ----===##
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+#
+# ===----------------------------------------------------------------------===#
+import subprocess
+import platform
+import os
+import sys
+
+from test_utils import *
+
+def setup_test():
+    change_dir(test_config.current_test)
+    return True
+
+def migrate_test():
+    call_subprocess(test_config.CT_TOOL + " a.cu -rule-file=./ruleaaa.yaml --cuda-include-path=" + test_config.include_path)
+    return is_sub_string("Error: failed to read", test_config.command_output)
+
+def build_test():
+    return True
+
+def run_test():
+    return True
