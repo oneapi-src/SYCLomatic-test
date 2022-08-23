e908ee3c3f104e60b6eb91a4da0e562ea7b9bdcb
Cui, Dele <dele.cui@intel.com>
2022-08-23 15:34:21 +0800

    [SYCLomatic] Add behavior tests for option --analysis-scope-path (#63)
    Signed-off-by: Cui, Dele <dele.cui@intel.com>

---
+++ behavior_tests/src/bt-analysis-scope-path1/cuda/call_device_func_outside.cu
@@ -0,0 +1,14 @@
+#include "../utils.cuh"
+
+__global__ void kernel(float *input)
+{
+  float sum = 0;
+  __shared__ float smem[128];
+  float total_sum = BlockReduceSum(sum, smem);
+}
+
+void foo()
+{
+  float *input = NULL;
+  kernel<<<1, 128>>>(input);
+}
---
+++ behavior_tests/src/bt-analysis-scope-path3/cuda/call_device_func_outside.cu
@@ -0,0 +1,15 @@
+#include "../inc/utils.cuh"
+#include "../inc/empty.h"
+
+__global__ void kernel(float *input)
+{
+  float sum = 0;
+  __shared__ float smem[128];
+  float total_sum = BlockReduceSum(sum, smem);
+}
+
+void foo()
+{
+  float *input = NULL;
+  kernel<<<1, 128>>>(input);
+}
