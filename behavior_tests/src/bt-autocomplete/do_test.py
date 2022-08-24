4f59ff75214dda505a5c4bb247968a8b19993642
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2022-08-24 11:16:04 +0800

    Update
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- behavior_tests/src/bt-autocomplete/do_test.py
+++ behavior_tests/src/bt-autocomplete/do_test.py
@@ -1,9 +1,8 @@
-# ====------ do_test.py---------- *- Python -* ----===##
+# ===------- do_test.py--------------------------------- *- Python -* -----===#
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-#
 #
 # ===----------------------------------------------------------------------===#
 import subprocess
