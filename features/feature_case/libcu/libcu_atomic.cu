5197c2698104131853f47358b938d94d6cea7471
Ni, Wenhui <wenhui.ni@intel.com>
2022-09-07 14:45:28 +0800

    add atomic_thread_fence test in feature
    Signed-off-by: Ni, Wenhui <wenhui.ni@intel.com>

--- features/feature_case/libcu/libcu_atomic.cu
+++ features/feature_case/libcu/libcu_atomic.cu
@@ -1,5 +1,11 @@
 #include <cuda/atomic>
 
+__global__ void example_kernel(int* data) {
+  *data = 42;
+  cuda::atomic_thread_fence(cuda::std::memory_order_release,
+                            cuda::thread_scope_device);
+}
+
 int main(){
   cuda::atomic<int> a;
   cuda::atomic<int> b(0);
@@ -15,5 +21,8 @@ int main(){
 
   ans = a.fetch_add(1);
   ans = a.fetch_sub(-1);
+
+  int * data = new int(0);
+  example_kernel(data) ;
   return 0;
 }
