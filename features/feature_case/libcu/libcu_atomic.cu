4f6dc49d2dff805ebc26684f40be921fc01a7b00
Ni, Wenhui <wenhui.ni@intel.com>
2022-09-09 08:33:41 +0800

    add comments
    Signed-off-by: Ni, Wenhui <wenhui.ni@intel.com>

--- features/feature_case/libcu/libcu_atomic.cu
+++ features/feature_case/libcu/libcu_atomic.cu
@@ -1,3 +1,11 @@
+// ====------ libcu_atomic.cpp----------- *- CUDA -*  ----===////
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//
+// ===----------------------------------------------------------------------===//
 #include <cuda/atomic>
 
 __global__ void example_kernel(int* data) {
