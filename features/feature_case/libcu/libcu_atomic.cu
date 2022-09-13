a9664856a5eebe8c1202bd10b6b442eeabdea21d
Ni, Wenhui <wenhui.ni@intel.com>
2022-09-13 16:18:57 +0800

    disable test case of libcu before cuda113
    Signed-off-by: Ni, Wenhui <wenhui.ni@intel.com>

--- features/feature_case/libcu/libcu_atomic.cu
+++ features/feature_case/libcu/libcu_atomic.cu
@@ -1,4 +1,4 @@
-// ====------ libcu_atomic.cpp----------- *- CUDA -*  ----===////
+// ====------ libcu_atomic.cu----------- *- CUDA -*  ----===////
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
