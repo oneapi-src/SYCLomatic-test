ab39627e93b0d648ca519a8d84bb4ebb7714f405
Cui, Dele <dele.cui@intel.com>
2022-09-17 21:44:02 +0800

    [SYCLomatic] Update check-windows-version to check dpct.exe's visible properties
    Signed-off-by: Cui, Dele <dele.cui@intel.com>

--- behavior_tests/src/check-windows-version/do_test.py
+++ behavior_tests/src/check-windows-version/do_test.py
@@ -10,7 +10,7 @@ import subprocess
 import platform
 import os
 import sys
-from test_config import CT_TOOL
+import test_config
 
 from test_utils import *
 
@@ -21,12 +21,34 @@ def setup_test():
 def get_windows_version(arg1, arg2):
     call_subprocess("powershell \"(Get-Item -path " + arg1 + ").VersionInfo." + arg2 + "\"")
     return test_config.command_output
-def migrate_test():
 
-    version = "2022.0.0.0"
-    call_subprocess(test_config.CT_TOOL + " --cuda-include-path=" + test_config.include_path +
-        " --usm-level=abc")
-    return is_sub_string("--usm-level option: Cannot find option named \'abc\'", test_config.command_output)
+def migrate_test():
+    ct_path = get_ct_path()
+    if not ct_path:
+        print('Cannot find the path to dpct!')
+        return False
+    ct_clang_version = get_ct_clang_version()
+    if not ct_path:
+        print("Cannot find dpct's bundled clang version!")
+        return False
+    print("dpct's bundled clang version is: {}".format(ct_clang_version))
+
+    file_version = get_windows_version(ct_path, 'FileVersion').strip()
+    product_version = get_windows_version(ct_path, 'ProductVersion').strip()
+    product_name = get_windows_version(ct_path, 'ProductName').strip()
+    file_description = get_windows_version(ct_path, 'FileDescription').strip()
+    legal_copyright = get_windows_version(ct_path, 'LegalCopyright').strip()
+
+    print("====={}'s VersionInfo properties=====".format(ct_path))
+    print("FileVersion: {}".format(file_version))
+    print("ProductVersion: {}".format(product_version))
+    print("ProductName: {}".format(product_name))
+    print("FileDescription: {}".format(file_description))
+    print("LegalCopyright: {}".format(legal_copyright))
+    print("==========")
+
+    return file_version == ct_clang_version and product_version == ct_clang_version and \
+        product_name == "SYCLomatic" and file_description == "" and legal_copyright == ""
 
 def build_test():
     return True
