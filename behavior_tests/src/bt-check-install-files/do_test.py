7a824584823d6f814deab9c3c42502d265097145
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2022-10-18 15:32:30 +0800

    Update error message
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- behavior_tests/src/bt-check-install-files/do_test.py
+++ behavior_tests/src/bt-check-install-files/do_test.py
@@ -22,9 +22,15 @@ def migrate_test():
     install_root = os.path.join(os.path.dirname(shutil.which("dpct")), '..')
 
     res = True
-    res = res and os.path.isfile(os.path.join(install_root, 'env/bash-autocomplete.sh'))
-    res = res and os.path.isfile(os.path.join(install_root, 'extensions/opt_rules/forceinline.yaml'))
-    res = res and os.path.isfile(os.path.join(install_root, 'extensions/opt_rules/intel_specific_math.yaml'))
+    if not os.path.isfile(os.path.join(install_root, 'env/bash-autocomplete.sh')):
+        print('Cannot found file:' + os.path.join(install_root, 'env/bash-autocomplete.sh'))
+        res = False
+    if not os.path.isfile(os.path.join(install_root, 'extensions/opt_rules/forceinline.yaml')):
+        print('Cannot found file:' + os.path.join(install_root, 'extensions/opt_rules/forceinline.yaml'))
+        res = False
+    if not os.path.isfile(os.path.join(install_root, 'extensions/opt_rules/intel_specific_math.yaml')):
+        print('Cannot found file:' + os.path.join(install_root, 'extensions/opt_rules/intel_specific_math.yaml'))
+        res = False
 
     return res
 
