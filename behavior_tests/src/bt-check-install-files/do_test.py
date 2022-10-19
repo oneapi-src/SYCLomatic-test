1ebb6c738b49e3c1854488320aa2c4c7257dea83
zhiweij1 <zhiwei.jiang@intel.com>
2022-10-19 13:45:41 +0800

    [SYCLomatic] Check configure files in the install folder (#114)
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

---
+++ behavior_tests/src/bt-check-install-files/do_test.py
@@ -0,0 +1,43 @@
+# ====------ do_test.py---------- *- Python -* ----===##
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+#
+# ===----------------------------------------------------------------------===#
+import subprocess
+import platform
+import os
+import sys
+
+from test_utils import *
+from hashlib import md5
+
+def setup_test():
+    change_dir(test_config.current_test)
+    return True
+
+def migrate_test():
+    install_root = os.path.join(os.path.dirname(shutil.which("dpct")), '..')
+
+    res = True
+    if platform.system() == 'Linux':
+      if not os.path.isfile(os.path.join(install_root, 'env/bash-autocomplete.sh')):
+          print('Cannot found file:' + os.path.join(install_root, 'env/bash-autocomplete.sh'))
+          res = False
+    if not os.path.isfile(os.path.join(install_root, 'extensions/opt_rules/forceinline.yaml')):
+        print('Cannot found file:' + os.path.join(install_root, 'extensions/opt_rules/forceinline.yaml'))
+        res = False
+    if not os.path.isfile(os.path.join(install_root, 'extensions/opt_rules/intel_specific_math.yaml')):
+        print('Cannot found file:' + os.path.join(install_root, 'extensions/opt_rules/intel_specific_math.yaml'))
+        res = False
+
+    return res
+
+def build_test():
+    return True
+
+def run_test():
+    return True
+
