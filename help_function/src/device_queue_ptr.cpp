0bcda401c2e92c27f702e3c169624316c2f36dc6
Cai, Justin <justin.cai@intel.com>
2022-11-07 08:34:07 -0800

    Add queue_ptr -> int -> queue_ptr test

--- help_function/src/device_queue_ptr.cpp
+++ help_function/src/device_queue_ptr.cpp
@@ -21,12 +21,17 @@ int main() {
 
   auto& q = dpct::get_default_queue();
   auto is_default = [&q](auto p) { return &q == &*p; };
+
   dpct::queue_ptr p1(0);
   dpct::queue_ptr p2(nullptr);
+  int x = p1;
+  dpct::queue_ptr p3(x);
+
   dpct::queue_ptr arr[] = {0, 0U, 0L, 0UL, 0LL, 0ULL};
 
   check("queue_ptr(0) is default", is_default(p1));
   check("queue_ptr(nullptr) is default", is_default(p2));
+  check("queue_ptr(x) is default", is_default(p3));
   check("queue_wrapper is default", is_default(queue_wrapper().p));
 
   foo(0);
