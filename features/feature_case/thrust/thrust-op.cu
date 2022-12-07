d431c2afa9be1a4d478ce49f06ac8ffaf6adb6f3
Ni, Wenhui <wenhui.ni@intel.com>
2022-12-07 21:29:51 +0800

    enable thrust_op test
    Signed-off-by: Ni, Wenhui <wenhui.ni@intel.com>

--- features/feature_case/thrust/thrust-op.cu
+++ features/feature_case/thrust/thrust-op.cu
@@ -9,28 +9,38 @@
 #include <thrust/functional.h>
 #include <iostream>
 
-__global__ void compareop_test(int *res)
+__global__ void compareop_kernel(int *res)
 {
-  if (thrust::greater_equal(*res, *(res + 1)))
+  if (thrust::greater_equal<>()(*res, *(res + 1)))
     *(res + 2) += 1;
-  if (thrust::greater_equal(*res + 1, *(res + 1)))
+  if (thrust::greater_equal<>()(*res + 1, *(res + 1)))
     *(res + 2) += 1;
-  if (thrust::greater_equal(*res, *(res + 1) + 1))
+  if (thrust::greater_equal<>()(*res, *(res + 1) + 1))
     *(res + 2) += 1;
   //*(res + 2) == 2
 
-  if (thrust::less_equal(*res, *(res + 1)))
+  if (thrust::less_equal<>()(*res, *(res + 1)))
     *(res + 2) += 1;
-  if (thrust::less_equal(*res + 1, *(res + 1)))
+  if (thrust::less_equal<>()(*res + 1, *(res + 1)))
     *(res + 2) += 1;
-  if (thrust::less_equal(*res, *(res + 1) + 1))
+  if (thrust::less_equal<>()(*res, *(res + 1) + 1))
     *(res + 2) += 1;
   //*(res + 2) == 4
 
-  
+  if (thrust::logical_and<>()(*res, *(res + 1)))
+    *(res + 2) += 1;
+  if (thrust::logical_and<>()(*res + 1, *(res + 1)))
+    *(res + 2) += 1;
+  if (thrust::logical_and<>()(*res, *(res + 1) + 1))
+    *(res + 2) += 1;
+  //*(res + 2) == 5
 
-
+  *(res + 2) += thrust::bit_and<>()(*res, *(res + 1));
+  *(res + 2) += thrust::bit_or<>()(*res, *(res + 1));
+  *(res + 2) += thrust::bit_xor<>()(*res, *(res + 1));
+  *(res + 2) += thrust::minimum<int>()(*res,*res);
 }
+
 
 int main()
 {
@@ -41,9 +51,14 @@ int main()
   int *deviceRes;
   cudaMalloc((int **)&deviceRes, 3 * sizeof(int));
   cudaMemcpy(deviceRes, hostRes, sizeof(int) * 3, cudaMemcpyHostToDevice);
-  compareop_test<<<1, 1>>>(deviceRes);
-
-  if ()
+  compareop_kernel<<<1, 1>>>(deviceRes);
+  cudaMemcpy(hostRes, deviceRes, sizeof(int) * 3, cudaMemcpyDeviceToHost);
+  cudaFree(deviceRes);
+  if (*hostRes==42&&*(hostRes+1)==42&&*(hostRes+2)==92)
+  {
+    free(hostRes);
     return 0;
+  }
+  free(hostRes);
   return 1;
 }
