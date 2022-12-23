78d43121348115dd440269e3cc87e2bd4c023e28
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2022-12-23 15:48:55 +0800

    [SYCLomatic #503] Test helper function dpct::fft::fft_engine::set_workspace
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

---
+++ help_function/src/fft_set_worksapce.cpp
@@ -0,0 +1,36 @@
+// ====------ fft_set_workspace.cpp ------------------------- *- C++ -* ----===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// ===----------------------------------------------------------------------===//
+
+#include <CL/sycl.hpp>
+#include <dpct/dpct.hpp>
+#include <cstdio>
+#include <oneapi/mkl.hpp>
+#include <dpct/fft_utils.hpp>
+
+void foo() {
+  dpct::fft::fft_engine *plan;
+  dpct::fft::fft_type type;
+  size_t workSize;
+  int batch = 1;
+  plan->create();
+  plan->use_internal_workspace(1);
+  plan->commit(&dpct::get_default_queue(), 3, type, batch, &workSize);
+  void *workArea;
+  plan->set_workspace(workArea);
+  dpct::fft::fft_engine::destroy(plan);
+}
+
+int main() {
+  try {
+    foo();
+  } catch (sycl::exception const &e) {
+    std::cerr << "SYCL exception: " << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
+}
