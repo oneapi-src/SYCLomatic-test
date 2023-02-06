fc4f3605b50d97f22a65e7a235ccd876792e9daa
chenwei.sun <chenwei.sun@intel.com>
2023-02-06 09:51:03 +0800

    test case updated
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- features/feature_case/thrust/thrust_raw_reference_cast.cu
+++ features/feature_case/thrust/thrust_raw_reference_cast.cu
@@ -1,4 +1,5 @@
-// ====------ thrust_raw_reference_cast.cu---------- *- CUDA -* -------------------===//
+// ====------ thrust_raw_reference_cast.cu---------- *- CUDA -*
+// -------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -14,10 +15,10 @@
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
 
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
 int main(void) {
-
-  int num_failures = 0;
-
   thrust::host_vector<int> h_vec(1);
   thrust::device_vector<int> d_vec = h_vec;
   d_vec[0] = 13;
@@ -25,25 +26,18 @@ int main(void) {
   thrust::device_reference<int> ref_to_thirteen = d_vec[0];
   int &ref = thrust::raw_reference_cast(ref_to_thirteen);
 
-  if (ref != 13) {
-    std::cout << "get_raw_reference test 1 failed.\n";
-    num_failures++;
+  int val = 0;
+  cudaError_t err = cudaMemcpy(&val, &ref, sizeof(int), cudaMemcpyDeviceToHost);
+
+  if (err != cudaSuccess) {
+    exit(EXIT_FAILURE);
   }
 
-  ref = 14;
-  if (ref_to_thirteen != 14) {
-    std::cout << "get_raw_reference test 2 failed.\n";
-    num_failures++;
+  if (val != 13) {
+    std::cout << "get_raw_reference test failed.\n";
+    return EXIT_FAILURE;
   }
 
-  int &ref2 = thrust::raw_reference_cast(ref);
-  ref2 = 15;
-  if (ref != 15) {
-    std::cout << "get_raw_reference test 3 failed.\n";
-    num_failures++;
-  }
-
-  if (num_failures == 0)
-    std::cout << "get_raw_reference test passed.\n";
-  return 0;
+  std::cout << "get_raw_reference test passed.\n";
+  return EXIT_SUCCESS;
 }
