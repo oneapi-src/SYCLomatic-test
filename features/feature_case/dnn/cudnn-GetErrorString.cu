3f0a6ce2cf5d31615726f0819f4f7b92c098de4e
Lu, John <john.lu@intel.com>
2023-02-14 02:22:20 +0000

    Initialize status string
    Signed-off-by: Lu, John <john.lu@intel.com>

--- features/feature_case/dnn/cudnn-GetErrorString.cu
+++ features/feature_case/dnn/cudnn-GetErrorString.cu
@@ -2,13 +2,13 @@
 #include <iostream>
 
 int main() {
-  cudnnStatus_t status;
+  cudnnStatus_t status{CUDNN_STATUS_SUCCESS};
 
   const char *msg=cudnnGetErrorString(status);
 
   if (!msg)
     return 1;
-    
+
   std::cout << "string = " << msg << "\n";
 
   return 0;
