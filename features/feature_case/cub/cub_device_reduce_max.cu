63122d3d1fd42e6b90305d484c46f68dc64aa376
Yihan Wang <yihan.wang@intel.com>
2023-02-14 09:36:51 +0800

    Remove redundant comments

--- features/feature_case/cub/cub_device_reduce_max.cu
+++ features/feature_case/cub/cub_device_reduce_max.cu
@@ -7,15 +7,6 @@
 //
 // ===----------------------------------------------------------------------===//
 
-// ====------ cub_device_reduce_min.cu-------------------- *- CUDA -* ------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//
-// ===----------------------------------------------------------------------===//
-
 #include <cub/cub.cuh>
 #include <initializer_list>
 #include <cstddef>
