4c422cfc403023f115019dc6c2ea09d83b1d9bed
0x12CC <68250218+0x12CC@users.noreply.github.com>
2023-03-23 18:13:13 -0400

    [SYCLomatic #732] Test ID and UUID migrations (#256)
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>

--- features/feature_case/device_info/device_info.cu
+++ features/feature_case/device_info/device_info.cu
@@ -7,14 +7,39 @@
 //
 // ===--------------------------------------------------------------===//
 
+#include <iostream>
 #include <stdio.h>
 
-int main() {
+void test0() {
   // no need to use `cudaSetDevice`
   size_t free_mem, total_mem;
   cudaMemGetInfo(&free_mem, &total_mem);
 
   printf("total_mem : [%lu]\n", total_mem);
   printf("free_mem  : [%lu]\n", free_mem);
-  return 0;
+}
+
+void test1() {
+  cudaDeviceProp properties;
+  cudaGetDeviceProperties(&properties, 0);
+
+  const int id = properties.pciDeviceID;
+  const cudaUUID_t uuid = properties.uuid;
+
+  properties.pciDeviceID = id;
+  properties.uuid = uuid;
+
+  std::cout << "Device ID: " << id << std::endl;
+  std::cout << "Device UUID: ";
+  for (int i = 0; i < 16; i++) {
+    std::cout << std::hex
+              << static_cast<int>(static_cast<unsigned char>(uuid.bytes[i]))
+              << " ";
+  }
+  std::cout << std::endl;
+}
+
+int main() {
+  test0();
+  test1();
 }
