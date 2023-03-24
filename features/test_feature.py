6443b49f1056ddead26336aac1c68d4ae3db7904
Lu, John <john.lu@intel.com>
2023-03-24 15:50:47 +0000

    [SYCLomatic #752] Test thrust::gather_if
    Signed-off-by: Lu, John <john.lu@intel.com>

--- features/test_feature.py
+++ features/test_feature.py
@@ -19,7 +19,9 @@ from test_utils import *
 
 exec_tests = ['thrust-vector-2', 'thrust-binary-search', 'thrust-count', 'thrust-copy',
               'thrust-qmc', 'thrust-transform-if', 'thrust-policy', 'thrust-list', 'module-kernel',
-              'kernel-launch', 'thrust-gather', 'thrust-scatter', 'thrust-unique_by_key_copy', 'thrust-for-hypre',
+              'kernel-launch', 'thrust-gather',
+              'thrust-gather_if',
+              'thrust-scatter', 'thrust-unique_by_key_copy', 'thrust-for-hypre',
               'thrust-rawptr-noneusm', 'driverStreamAndEvent', 'grid_sync', 'deviceProp', 'gridThreads', 'kernel_library', 'cub_block_p2',
               'cub_constant_iterator', 'cub_device_reduce_max', 'cub_device_reduce_min', 'cub_discard_iterator', 'ccl_test2'
               'cub_device', 'cub_device_reduce_sum', 'cub_device_reduce', 'cub_device_reduce_by_key', 'cub_device_select_unique_by_key',
