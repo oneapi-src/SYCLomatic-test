03b0cf7100f371d233f43c29e371a1af28ab1071
chenwei.sun <chenwei.sun@intel.com>
2023-03-30 09:02:00 +0800

    Format list case
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- features/feature_case/thrust/thrust_find_if.cu
+++ features/feature_case/thrust/thrust_find_if.cu
@@ -7,11 +7,10 @@
 //
 // ===-------------------------------------------------------------------===//
 
-#include <thrust/find.h>
-#include <thrust/execution_policy.h>
 #include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/find.h>
 #include <thrust/host_vector.h>
-
 
 struct greater_than_four {
   __host__ __device__ bool operator()(int x) const { return x > 4; }
@@ -19,9 +18,9 @@ struct greater_than_four {
 
 void test_1() {
 
-  int data[4] = {0,5, 3, 7};
+  int data[4] = {0, 5, 3, 7};
 
-  int *ptr = thrust::find_if(data, data+3, greater_than_four()); 
+  int *ptr = thrust::find_if(data, data + 3, greater_than_four());
 
   if (*ptr != 5) {
     std::cout << "test_1 run failed!\n";
@@ -31,14 +30,14 @@ void test_1() {
   std::cout << "test_1 run passed!\n";
 }
 
-
 void test_2() {
   const int N = 4;
   int data[4] = {0, 5, 3, 7};
   thrust::device_vector<int> device_data(data, data + N);
-  
+
   thrust::device_vector<int>::iterator iter;
-  iter = thrust::find_if(device_data.begin(), device_data.end(),  greater_than_four()); 
+  iter = thrust::find_if(device_data.begin(), device_data.end(),
+                         greater_than_four());
 
   if (*iter != 5) {
     std::cout << "test_2 run failed!\n";
@@ -52,9 +51,10 @@ void test_3() {
   const int N = 4;
   int data[4] = {0, 5, 3, 7};
   thrust::host_vector<int> host_data(data, data + N);
-  
+
   thrust::host_vector<int>::iterator iter;
-  iter = thrust::find_if(host_data.begin(), host_data.end(),  greater_than_four()); 
+  iter =
+      thrust::find_if(host_data.begin(), host_data.end(), greater_than_four());
 
   if (*iter != 5) {
     std::cout << "test_3 run failed!\n";
@@ -64,13 +64,11 @@ void test_3() {
   std::cout << "test_3 run passed!\n";
 }
 
-
-
 void test_4() {
 
-  int data[4] = {0,5, 3, 7};
+  int data[4] = {0, 5, 3, 7};
 
-  int *ptr = thrust::find_if(thrust::host, data, data+3, greater_than_four()); 
+  int *ptr = thrust::find_if(thrust::host, data, data + 3, greater_than_four());
 
   if (*ptr != 5) {
     std::cout << "test_4 run failed!\n";
@@ -80,14 +78,14 @@ void test_4() {
   std::cout << "test_4 run passed!\n";
 }
 
-
 void test_5() {
   const int N = 4;
   int data[4] = {0, 5, 3, 7};
   thrust::device_vector<int> device_data(data, data + N);
-  
+
   thrust::device_vector<int>::iterator iter;
-  iter = thrust::find_if(thrust::device, device_data.begin(), device_data.end(),  greater_than_four()); 
+  iter = thrust::find_if(thrust::device, device_data.begin(), device_data.end(),
+                         greater_than_four());
 
   if (*iter != 5) {
     std::cout << "test_5 run failed!\n";
@@ -97,14 +95,14 @@ void test_5() {
   std::cout << "test_5 run passed!\n";
 }
 
-
 void test_6() {
   const int N = 4;
   int data[4] = {0, 5, 3, 7};
   thrust::host_vector<int> host_data(data, data + N);
-  
+
   thrust::host_vector<int>::iterator iter;
-  iter = thrust::find_if(thrust::host, host_data.begin(), host_data.end(),  greater_than_four()); 
+  iter = thrust::find_if(thrust::host, host_data.begin(), host_data.end(),
+                         greater_than_four());
 
   if (*iter != 5) {
     std::cout << "test_6 run failed!\n";
@@ -113,7 +111,6 @@ void test_6() {
 
   std::cout << "test_6 run passed!\n";
 }
-
 
 int main() {
 
