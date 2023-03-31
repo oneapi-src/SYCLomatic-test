9b576f8af782a7c22cdf6717852312891b0cb190
tomflinda <chenwei.sun@intel.com>
2023-03-31 14:53:37 +0800

    [SYCLomatic #777] Add test for migration of thrust::find_if and thrust::find_if_not (#276)
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- features/test_feature.py
+++ features/test_feature.py
@@ -45,7 +45,8 @@ exec_tests = ['thrust-vector-2', 'thrust-binary-search', 'thrust-count', 'thrust
               'thrust_set_difference', 'thrust_set_difference_by_key', 'thrust_set_intersection_by_key', 'thrust_stable_sort',
               'thrust_tabulate', 'thrust_for_each_n', 'device_info', 'defaultStream', 'cudnn-rnn', 'feature_profiling',
               'thrust_raw_reference_cast', 'thrust_partition_copy', 'thrust_stable_partition_copy',
-              'thrust_stable_partition', 'thrust_remove', 'cub_device_segmented_sort_pairs']
+              'thrust_stable_partition', 'thrust_remove', 'cub_device_segmented_sort_pairs', 'thrust_find_if_not',
+              'thrust_find_if']
 
 def setup_test():
     return True
