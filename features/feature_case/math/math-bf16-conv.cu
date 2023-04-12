597f16bb235ece08d106693a58192efe33efa9d6
Tang, Jiajun <jiajun.tang@intel.com>
2023-04-12 10:46:20 +0800

    [SYCLomatic] Aviod malloc bf16 on host.
    Signed-off-by: Tang, Jiajun jiajun.tang@intel.com

--- features/feature_case/math/math-bf16-conv.cu
+++ features/feature_case/math/math-bf16-conv.cu
@@ -17,8 +17,6 @@ using namespace std;
 
 typedef pair<float2, int> f2i_pair;
 typedef pair<float, int> fi_pair;
-typedef pair<__nv_bfloat162, int> bf2i_pair;
-typedef pair<__nv_bfloat16, int> bfi_pair;
 
 int passed = 0;
 int failed = 0;
@@ -52,7 +50,27 @@ void checkResult(const string &FuncName, const vector<__nv_bfloat162> &Inputs,
         abs(Result.y - Expect.y) < pow(10, -precision));
 }
 
-void checkResult(const string &FuncName, const vector<__nv_bfloat16> &Inputs,
+void checkResult(const string &FuncName, const vector<float2> &Inputs,
+                 const float2 &Expect, const float2 &Result,
+                 const int precision) {
+  cout << FuncName << "({" << __bfloat162float(Inputs[0].x) << ", "
+       << __bfloat162float(Inputs[0].y) << "}";
+  for (size_t i = 1; i < Inputs.size(); ++i) {
+    cout << ", {" << __bfloat162float(Inputs[i].x) << ", "
+         << __bfloat162float(Inputs[i].y) << "}";
+  }
+  cout << ") = " << fixed << setprecision(precision) << "{" << Result.x << ", "
+       << Result.y << "} (expect {" << Expect.x - pow(10, -precision) << " ~ "
+       << Expect.x + pow(10, -precision) << ", "
+       << Expect.y - pow(10, -precision) << " ~ "
+       << Expect.y + pow(10, -precision) << ")";
+  cout.unsetf(ios::fixed);
+  check(abs(Result.x - Expect.x) < pow(10, -precision) &&
+        abs(Result.y - Expect.y) < pow(10, -precision));
+}
+
+template <typename T = float>
+void checkResult(const string &FuncName, const vector<T> &Inputs,
                  const float &Expect, const float &Result,
                  const int precision) {
   cout << FuncName << "(" << __bfloat162float(Inputs[0]) << "";
@@ -64,41 +82,6 @@ void checkResult(const string &FuncName, const vector<__nv_bfloat16> &Inputs,
        << ")";
   cout.unsetf(ios::fixed);
   check(abs(Result - Expect) < pow(10, -precision));
-}
-
-void checkResult(const string &FuncName, const vector<float2> &Inputs,
-                 const __nv_bfloat162 &Expect, const __nv_bfloat162 &Result,
-                 const int precision) {
-  cout << FuncName << "({" << Inputs[0].x << ", " << Inputs[0].y << "}";
-  for (size_t i = 1; i < Inputs.size(); ++i) {
-    cout << ", {" << Inputs[i].x << ", " << Inputs[i].y << "}";
-  }
-  cout << ") = " << fixed << setprecision(precision) << "{"
-       << __bfloat162float(Result.x) << ", " << __bfloat162float(Result.y)
-       << "} (expect {" << __bfloat162float(Expect.x) - pow(10, -precision)
-       << " ~ " << __bfloat162float(Expect.x) + pow(10, -precision) << ", "
-       << __bfloat162float(Expect.y) - pow(10, -precision) << " ~ "
-       << __bfloat162float(Expect.y) + pow(10, -precision) << ")";
-  cout.unsetf(ios::fixed);
-  check(abs(__bfloat162float(Result.x) - __bfloat162float(Expect.x)) <
-            pow(10, -precision) &&
-        abs(__bfloat162float(Result.y) - __bfloat162float(Expect.y)) <
-            pow(10, -precision));
-}
-
-void checkResult(const string &FuncName, const vector<float> &Inputs,
-                 const __nv_bfloat16 &Expect, const __nv_bfloat16 &Result,
-                 const int precision) {
-  cout << FuncName << "(" << Inputs[0] << "";
-  for (size_t i = 1; i < Inputs.size(); ++i) {
-    cout << ", " << Inputs[i];
-  }
-  cout << ") = " << fixed << setprecision(precision) << __bfloat162float(Result)
-       << " (expect " << __bfloat162float(Expect) - pow(10, -precision) << " ~ "
-       << __bfloat162float(Expect) + pow(10, -precision) << ")";
-  cout.unsetf(ios::fixed);
-  check(abs(__bfloat162float(Result) - __bfloat162float(Expect)) <
-        pow(10, -precision));
 }
 
 __global__ void bFloat1622float2(float *const Result, __nv_bfloat162 Input1) {
@@ -144,16 +127,15 @@ void testBFloat162floatCases(
   }
 }
 
-__global__ void float22bFloat162_rn(__nv_bfloat16 *const Result,
-                                    float2 Input1) {
+__global__ void float22bFloat162_rn(float *const Result, float2 Input1) {
   auto ret = __float22bfloat162_rn(Input1);
-  Result[0] = ret.x;
-  Result[1] = ret.y;
+  Result[0] = __bfloat162float(ret.x);
+  Result[1] = __bfloat162float(ret.y);
 }
 
 void testFloat22bFloat162_rnCases(
-    const vector<pair<float2, bf2i_pair>> &TestCases) {
-  __nv_bfloat16 *Result;
+    const vector<pair<float2, f2i_pair>> &TestCases) {
+  float *Result;
   cudaMallocManaged(&Result, 2 * sizeof(*Result));
   for (const auto &TestCase : TestCases) {
     float22bFloat162_rn<<<1, 1>>>(Result, TestCase.first);
@@ -170,12 +152,12 @@ void testFloat22bFloat162_rnCases(
   }
 }
 
-__global__ void float2bFloat16(__nv_bfloat16 *const Result, float Input1) {
-  *Result = __float2bfloat16(Input1);
+__global__ void float2bFloat16(float *const Result, float Input1) {
+  *Result = __bfloat162float(__float2bfloat16(Input1));
 }
 
-void testFloat2bFloat16Cases(const vector<pair<float, bfi_pair>> &TestCases) {
-  __nv_bfloat16 *Result;
+void testFloat2bFloat16Cases(const vector<pair<float, fi_pair>> &TestCases) {
+  float *Result;
   cudaMallocManaged(&Result, sizeof(*Result));
   for (const auto &TestCase : TestCases) {
     float2bFloat16<<<1, 1>>>(Result, TestCase.first);
