253ef74e1bfe3f24ce382ab0e39ef761441670fe
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-05-04 10:00:14 +0800

    Add license
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- features/feature_case/cooperative_groups/cooperative_groups_reduce.cu
+++ features/feature_case/cooperative_groups/cooperative_groups_reduce.cu
@@ -1,3 +1,11 @@
+// ===------- cooperative_groups_reduce.cu --------------- *- CUDA -* ------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// ===----------------------------------------------------------------------===//
+
 #include <cooperative_groups.h>
 #include <cooperative_groups/reduce.h>
 #include <iostream>
