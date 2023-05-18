726a462a0199c436395b30b2a8b1ad5142bfe3af
Dan Hoeflinger <dan.hoeflinger@intel.com>
2023-05-18 17:10:08 -0400

    Adding test coverage for device_vector
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- help_function/src/assign_device_vector.cpp
+++ help_function/src/assign_device_vector.cpp
@@ -4,6 +4,73 @@
 #include <dpct/dpct.hpp>
 #include <dpct/dpl_utils.hpp>
 
+template <typename Vector>
+bool verify(Vector &D, int N, int V) {
+  if (D.size() != N)
+  {
+    std::cout<<"size mismatch"<<std::endl;
+    return false;
+  }
+  for (int i = 0; i < N; ++i)
+    if (D[i] != V)
+    {
+      std::cout<<"value mismatch "<<D[i]<< " != "<<V<<std::endl;
+      return false;
+    }
+  return true;
+}
+
+
+static int num_constructed_prop = 0;
+static int num_destroyed_prop = 0;
+static int num_constructed_no_prop = 0;
+static int num_destroyed_no_prop = 0;
+
+template <typename T>
+class AllocWithNoCopyPropagation : public sycl::usm_allocator<T, sycl::usm::alloc::shared> {
+  public:
+  AllocWithNoCopyPropagation(const sycl::context &Ctxt, const sycl::device &Dev,
+                                     const sycl::property_list &PropList = {}) 
+                       : sycl::usm_allocator<T, sycl::usm::alloc::shared>(Ctxt, Dev, PropList)
+  {}
+  AllocWithNoCopyPropagation(const sycl::queue &Q, const sycl::property_list &PropList = {}) 
+    : sycl::usm_allocator<T, sycl::usm::alloc::shared>(Q, PropList)
+  {}
+  
+  AllocWithNoCopyPropagation(const AllocWithNoCopyPropagation& other)
+    : sycl::usm_allocator<T, sycl::usm::alloc::shared>(other)
+  {}
+
+  typedef ::std::false_type propagate_on_container_copy_assignment;
+  static void construct(T *p) { ::new((void*)p) T();  num_constructed_no_prop++;}
+  template <typename _Arg>
+  static void construct(T *p, _Arg arg) { ::new((void*)p) T(arg); num_constructed_no_prop++;}
+  static void destroy(T *p) { p->~T(); num_destroyed_no_prop++;}
+  
+};
+
+template <typename T>
+class AllocWithCopyPropagation : public sycl::usm_allocator<T, sycl::usm::alloc::shared> {
+  public:
+  AllocWithCopyPropagation(const sycl::context &Ctxt, const sycl::device &Dev,
+                                     const sycl::property_list &PropList = {})
+                       : sycl::usm_allocator<T, sycl::usm::alloc::shared>(Ctxt, Dev, PropList)
+  {}
+  AllocWithCopyPropagation(const sycl::queue &Q, const sycl::property_list &PropList = {}) 
+    : sycl::usm_allocator<T, sycl::usm::alloc::shared>(Q, PropList)
+  {}
+  
+  AllocWithCopyPropagation(const AllocWithCopyPropagation& other)
+    : sycl::usm_allocator<T, sycl::usm::alloc::shared>(other)
+  {}
+
+  typedef ::std::true_type propagate_on_container_copy_assignment;
+  static void construct(T *p) { ::new((void*)p) T();  num_constructed_prop++;}
+  template <typename _Arg>
+  static void construct(T *p, _Arg arg) { ::new((void*)p) T(arg); num_constructed_prop++;}
+  static void destroy(T *p) { p->~T(); num_destroyed_prop++;}
+};
+
 int main(void)
 {
     // H has storage for 4 integers
@@ -23,8 +90,59 @@ int main(void)
     D[1] = 88;
     
     H = D;
-    if (H[0] == 99 && H[1] == 88)
-      return 0;
-    else
+    if (H[0] != 99 || H[1] != 88)
       return 1;
+
+    constexpr int N = 5;
+    constexpr int V = 99;
+    dpct::device_vector<int> D1(N, V);
+
+    AllocWithNoCopyPropagation<int> alloc_no_copy_prop1(dpct::get_default_queue());
+    AllocWithNoCopyPropagation<int> alloc_no_copy_prop2(dpct::get_default_queue());
+    
+    //should construct 5 ints from D1 in the new allocation space
+    dpct::device_vector<int, AllocWithNoCopyPropagation<int>> D2(std::move(D1), alloc_no_copy_prop1);
+    if (!verify(D2, N, V)) {
+        std::cout<<"Failed move of default allocator to AllocWithNoCopyProp"<<std::endl;
+        return 1;
+    }
+    //Should allocate and construct 10 int{}
+    dpct::device_vector<int, AllocWithNoCopyPropagation<int>> D3(10, alloc_no_copy_prop2);
+    //since there is no copy propagation, we only destroy the excess, not all 10 elements, and we copy the N elements
+    D3 = D2; 
+    if (!verify(D3, N, V)) {
+        std::cout<<"Failed move assign of AllocWithNoCopyProp"<<std::endl;
+        return 1;
+    }
+
+    if (num_constructed_no_prop != 15 && num_destroyed_no_prop != 5)
+    {
+        std::cout<<"Allocator without copy propagation is copying incorrectly: ["<<num_constructed_no_prop<<", "<<num_destroyed_no_prop<<"]"<<std::endl;
+        return 1;
+    }
+
+    AllocWithCopyPropagation<int> alloc_copy_prop1(dpct::get_default_queue());
+    AllocWithCopyPropagation<int> alloc_copy_prop2(dpct::get_default_queue());
+    
+    //Should allocate 5 ints from D3 in the new allocation space
+    dpct::device_vector<int, AllocWithCopyPropagation<int>> D4(std::move(D3), alloc_copy_prop1);
+    if (!verify(D4, N, V)) {
+        std::cout<<"Failed move of AllocWithNoCopyProp to AllocWithCopyProp"<<std::endl;
+        return 1;
+    }
+    //Should allocate and construct 10 int{}
+    dpct::device_vector<int, AllocWithCopyPropagation<int>> D5(10, alloc_copy_prop2);
+    //since there is copy propagation, we destroy all 10 elements, and use the propagated allocator to construct
+    // 5 new elements
+    D5 = D4; 
+    if (!verify(D5, N, V)) {
+        std::cout<<"Failed copy assign of AllocWithCopyProp"<<std::endl;
+        return 1;
+    }
+
+    if (num_constructed_prop != 20 && num_destroyed_prop != 10)
+    {
+        std::cout<<"Allocator with copy propagation is copying incorrectly: ["<<num_constructed_prop<<", "<<num_destroyed_prop<<"]"<<std::endl;
+        return 1;
+    }
 }
--- help_function/src/move_device_vector.cpp
+++ help_function/src/move_device_vector.cpp
@@ -4,14 +4,72 @@
 #include <dpct/dpct.hpp>
 #include <dpct/dpl_utils.hpp>
 
-bool verify(dpct::device_vector<int> &D, int N, int V) {
+template <typename Vector>
+bool verify(Vector &D, int N, int V) {
   if (D.size() != N)
+  {
+    std::cout<<"size mismatch"<<std::endl;
     return false;
+  }
   for (int i = 0; i < N; ++i)
     if (D[i] != V)
+    {
+      std::cout<<"value mismatch "<<D[i]<< " != "<<V<<std::endl;
       return false;
+    }
   return true;
 }
+
+static int num_constructed_prop = 0;
+static int num_destroyed_prop = 0;
+static int num_constructed_no_prop = 0;
+static int num_destroyed_no_prop = 0;
+
+template <typename T>
+class AllocWithNoMovePropagation : public sycl::usm_allocator<T, sycl::usm::alloc::shared> {
+  public:
+  AllocWithNoMovePropagation(const sycl::context &Ctxt, const sycl::device &Dev,
+                                     const sycl::property_list &PropList = {}) 
+                       : sycl::usm_allocator<T, sycl::usm::alloc::shared>(Ctxt, Dev, PropList)
+  {}
+  AllocWithNoMovePropagation(const sycl::queue &Q, const sycl::property_list &PropList = {}) 
+    : sycl::usm_allocator<T, sycl::usm::alloc::shared>(Q, PropList)
+  {}
+  
+  AllocWithNoMovePropagation(const AllocWithNoMovePropagation& other)
+    : sycl::usm_allocator<T, sycl::usm::alloc::shared>(other)
+  {}
+
+  typedef ::std::false_type propagate_on_container_move_assignment;
+  static void construct(T *p) { ::new((void*)p) T();  num_constructed_no_prop++;}
+  template <typename _Arg>
+  static void construct(T *p, _Arg arg) { ::new((void*)p) T(arg); num_constructed_no_prop++;}
+  static void destroy(T *p) { p->~T(); num_destroyed_no_prop++;}
+  
+};
+
+template <typename T>
+class AllocWithMovePropagation : public sycl::usm_allocator<T, sycl::usm::alloc::shared> {
+  public:
+  AllocWithMovePropagation(const sycl::context &Ctxt, const sycl::device &Dev,
+                                     const sycl::property_list &PropList = {})
+                       : sycl::usm_allocator<T, sycl::usm::alloc::shared>(Ctxt, Dev, PropList)
+  {}
+  AllocWithMovePropagation(const sycl::queue &Q, const sycl::property_list &PropList = {}) 
+    : sycl::usm_allocator<T, sycl::usm::alloc::shared>(Q, PropList)
+  {}
+  
+  AllocWithMovePropagation(const AllocWithMovePropagation& other)
+    : sycl::usm_allocator<T, sycl::usm::alloc::shared>(other)
+  {}
+
+  typedef ::std::true_type propagate_on_container_move_assignment;
+  static void construct(T *p) { ::new((void*)p) T();  num_constructed_prop++;}
+  template <typename _Arg>
+  static void construct(T *p, _Arg arg) { ::new((void*)p) T(arg); num_constructed_prop++;}
+  static void destroy(T *p) { p->~T(); num_destroyed_prop++;}
+};
+
 
 int main(void) {
   constexpr int N = 4;
@@ -23,9 +81,59 @@ int main(void) {
   }
   // Move assign to D2.
   dpct::device_vector<int> D2;
+  auto alloc = D2.get_allocator();
   D2 = std::move(dpct::device_vector<int>(N, V));
   if (!verify(D2, N, V)) {
     return 1;
   }
+  AllocWithNoMovePropagation<int> alloc_no_move_prop(dpct::get_default_queue());
+  
+  dpct::device_vector<int, AllocWithNoMovePropagation<int>> D3(std::move(D2), alloc_no_move_prop);
+  if (!verify(D3, N, V)) {
+    std::cout<<"Failed move of default allocator to AllocWithNoMoveProp"<<std::endl;
+    return 1;
+  }
+  dpct::device_vector<int, AllocWithNoMovePropagation<int>> D4(std::move(D3));
+  if (!verify(D4, N, V)) {
+    std::cout<<"Failed move of AllocWithNoMoveProp"<<std::endl;
+    return 1;
+  }
+  dpct::device_vector<int, AllocWithNoMovePropagation<int>> D5;
+  D5 = std::move(D4);
+  if (!verify(D5, N, V)) {
+    std::cout<<"Failed move assign of AllocWithNoMoveProp"<<std::endl;
+    return 1;
+  }
+
+  if (num_constructed_no_prop != 8 && num_destroyed_no_prop != 4)
+  {
+    std::cout<<"Allocator without move propagation is moving incorrectly: ["<<num_constructed_no_prop<<", "<<num_destroyed_no_prop<<"]"<<std::endl;
+    return 1;
+  }
+
+  AllocWithMovePropagation<int> alloc_move_prop(dpct::get_default_queue());
+
+  dpct::device_vector<int, AllocWithMovePropagation<int>> D6(std::move(D5), alloc_move_prop);
+  if (!verify(D6, N, V)) {
+    std::cout<<"Failed move of AllocWithNoMoveProp to AllocWithMoveProp"<<std::endl;
+    return 1;
+  }
+  dpct::device_vector<int, AllocWithMovePropagation<int>> D7;
+  D7 = std::move(D6);
+  if (!verify(D7, N, V)) {
+    std::cout<<"Failed move assign of AllocWithMoveProp"<<std::endl;
+    return 1;
+  }
+  dpct::device_vector<int, AllocWithMovePropagation<int>> D8(std::move(D7));
+  if (!verify(D8, N, V)) {
+    std::cout<<"Failed move of AllocWithMoveProp"<<std::endl;
+    return 1;
+  }
+  if (num_constructed_prop != 4 && num_destroyed_prop != 0)
+  {
+    std::cout<<"Allocator with move propagation is moving incorrectly: ["<<num_constructed_prop<<", "<<num_destroyed_prop<<"]"<<std::endl;
+    return 1;
+  }
+
   return 0;
 }
