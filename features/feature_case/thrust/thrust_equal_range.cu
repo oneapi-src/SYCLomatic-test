440cc32cd8ea0cca06b0ed276a902d4b4ee85e34
chenwei.sun <chenwei.sun@intel.com>
2023-05-19 00:19:15 +0800

    Remove redundant comment line
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- features/feature_case/thrust/thrust_equal_range.cu
+++ features/feature_case/thrust/thrust_equal_range.cu
@@ -1,5 +1,4 @@
 // ====------ thrust_equal_range.cu------------- *- CUDA -*-----------------===//
-// -------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
