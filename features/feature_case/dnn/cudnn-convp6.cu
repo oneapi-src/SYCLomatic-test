93f9be4572cfc2635f665e6ffd1cb0a4c366dc15
Wang, Hao3 <hao3.wang@intel.com>
2023-05-22 10:31:21 +0800

    fix
    Signed-off-by: Wang, Hao3 <hao3.wang@intel.com>

--- features/feature_case/dnn/cudnn-convp6.cu
+++ features/feature_case/dnn/cudnn-convp6.cu
@@ -1,4 +1,4 @@
-// ====------ cudnn-convp6.cu ---------- *- CUDA -* ----===////
+// ====-------------------- cudnn-convp6.cu -------------- *- CUDA -* ----===////
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
