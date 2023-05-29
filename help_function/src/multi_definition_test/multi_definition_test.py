93969e386a047dc33b15db6694142edcc3ca451c
chenwei.sun <chenwei.sun@intel.com>
2023-05-30 01:23:55 +0800

    Comment addressed
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- help_function/src/multi_definition_test/multi_definition_test.py
+++ help_function/src/multi_definition_test/multi_definition_test.py
@@ -1,4 +1,4 @@
-# ====------ rodinia.py---------- *- Python -* ----===##
+# ====------ multi_definition_test.py------------------ *- Python -* ----===##
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
