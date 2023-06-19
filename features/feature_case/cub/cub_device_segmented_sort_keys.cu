70117c9b50a614c1d543afc2aeea95e6c130b55d
Wang, Yihan <yihan.wang@intel.com>
2023-06-19 16:25:58 +0800

    [SYCLomatic] Add license
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- features/feature_case/cub/cub_device_segmented_sort_keys.cu
+++ features/feature_case/cub/cub_device_segmented_sort_keys.cu
@@ -1,3 +1,12 @@
+// ===------- cub_device_segmented_sort_keys.cu ------------ *- CUDA -* ---===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//
+// ===---------------------------------------------------------------------===//
+
 #include <cstdlib>
 #include <cub/cub.cuh>
 #include <iostream>
