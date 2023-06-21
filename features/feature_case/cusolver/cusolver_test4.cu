edce6d9e67bdd026945f544149627d5d4541f6e5
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-06-21 10:34:59 +0800

    Remove duplicated test
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- features/feature_case/cusolver/cusolver_test4.cu
+++ features/feature_case/cusolver/cusolver_test4.cu
@@ -692,90 +692,6 @@ void test_cusolverDnSyevd() {
   }
 }
 
-void test_cusolverDnXtrtri() {
-  std::vector<float> a = {1, 2, 2, 4};
-  Data<float> a_s(a.data(), 4);
-  Data<double> a_d(a.data(), 4);
-  Data<float2> a_c(a.data(), 4);
-  Data<double2> a_z(a.data(), 4);
-
-  cusolverDnHandle_t handle;
-  cusolverDnCreate(&handle);
-
-  a_s.H2D();
-  a_d.H2D();
-  a_c.H2D();
-  a_z.H2D();
-
-  size_t lwork_s;
-  size_t lwork_d;
-  size_t lwork_c;
-  size_t lwork_z;
-  size_t lwork_host_s;
-  size_t lwork_host_d;
-  size_t lwork_host_c;
-  size_t lwork_host_z;
-
-  cusolverDnXtrtri_bufferSize(handle, CUBLAS_FILL_MODE_UPPER, CUBLAS_DIAG_NON_UNIT, 2, CUDA_R_32F, a_s.d_data, 2, &lwork_s, &lwork_host_s);
-  cusolverDnXtrtri_bufferSize(handle, CUBLAS_FILL_MODE_UPPER, CUBLAS_DIAG_NON_UNIT, 2, CUDA_R_64F, a_d.d_data, 2, &lwork_d, &lwork_host_d);
-  cusolverDnXtrtri_bufferSize(handle, CUBLAS_FILL_MODE_UPPER, CUBLAS_DIAG_NON_UNIT, 2, CUDA_C_32F, a_c.d_data, 2, &lwork_c, &lwork_host_c);
-  cusolverDnXtrtri_bufferSize(handle, CUBLAS_FILL_MODE_UPPER, CUBLAS_DIAG_NON_UNIT, 2, CUDA_C_64F, a_z.d_data, 2, &lwork_z, &lwork_host_z);
-
-  void* device_ws_s;
-  void* device_ws_d;
-  void* device_ws_c;
-  void* device_ws_z;
-  cudaMalloc(&device_ws_s, lwork_s);
-  cudaMalloc(&device_ws_d, lwork_d);
-  cudaMalloc(&device_ws_c, lwork_c);
-  cudaMalloc(&device_ws_z, lwork_z);
-  void* host_ws_s;
-  void* host_ws_d;
-  void* host_ws_c;
-  void* host_ws_z;
-  host_ws_s = malloc(lwork_host_s);
-  host_ws_d = malloc(lwork_host_d);
-  host_ws_c = malloc(lwork_host_c);
-  host_ws_z = malloc(lwork_host_z);
-
-  int *info;
-  cudaMalloc(&info, sizeof(int));
-
-  cusolverDnXtrtri(handle, CUBLAS_FILL_MODE_UPPER, CUBLAS_DIAG_NON_UNIT, 2, CUDA_R_32F, a_s.d_data, 2, device_ws_s, lwork_s, host_ws_s, lwork_host_s, info);
-  cusolverDnXtrtri(handle, CUBLAS_FILL_MODE_UPPER, CUBLAS_DIAG_NON_UNIT, 2, CUDA_R_64F, a_d.d_data, 2, device_ws_d, lwork_d, host_ws_d, lwork_host_d, info);
-  cusolverDnXtrtri(handle, CUBLAS_FILL_MODE_UPPER, CUBLAS_DIAG_NON_UNIT, 2, CUDA_C_32F, a_c.d_data, 2, device_ws_c, lwork_c, host_ws_c, lwork_host_c, info);
-  cusolverDnXtrtri(handle, CUBLAS_FILL_MODE_UPPER, CUBLAS_DIAG_NON_UNIT, 2, CUDA_C_64F, a_z.d_data, 2, device_ws_z, lwork_z, host_ws_z, lwork_host_z, info);
-
-  a_s.D2H();
-  a_d.D2H();
-  a_c.D2H();
-  a_z.D2H();
-
-  cudaStreamSynchronize(0);
-
-  cusolverDnDestroy(handle);
-  cudaFree(device_ws_s);
-  cudaFree(device_ws_d);
-  cudaFree(device_ws_c);
-  cudaFree(device_ws_z);
-  free(host_ws_s);
-  free(host_ws_d);
-  free(host_ws_c);
-  free(host_ws_z);
-  cudaFree(info);
-
-  float expect_a[4] = {1.000000,2.000000,-0.500000,0.250000};
-  if (compare_result(expect_a, a_s.h_data, 4) &&
-      compare_result(expect_a, a_d.h_data, 4) &&
-      compare_result(expect_a, a_c.h_data, 4) &&
-      compare_result(expect_a, a_z.h_data, 4))
-    printf("DnXtrtri pass\n");
-  else {
-    printf("DnXtrtri fail\n");
-    test_passed = false;
-  }
-}
-
 int main() {
   test_cusolverDnTsyevdx_cusolverDnTheevdx();
   test_cusolverDnTsygvdx_cusolverDnThegvdx();
@@ -783,7 +699,6 @@ int main() {
   test_cusolverDnTsyevj_cusolverDnTheevj();
   test_cusolverDnXsyevd();
   test_cusolverDnSyevd();
-  test_cusolverDnXtrtri();
 
   if (test_passed)
     return 0;
