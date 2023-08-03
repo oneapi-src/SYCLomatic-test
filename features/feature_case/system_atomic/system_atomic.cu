d51fc48374c0dd383fa1977c20b642ed5f527e0d
chenwei.sun <chenwei.sun@intel.com>
2023-08-04 06:39:47 +0800

    code comments updated
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- features/feature_case/system_atomic/system_atomic.cu
+++ features/feature_case/system_atomic/system_atomic.cu
@@ -1,4 +1,4 @@
-// ====------ atomic_test.cu---------- *- CUDA -* ----===////
+// ====------------------------ system_atomic.cu----------- *- CUDA -* ----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
