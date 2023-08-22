cf3b8e523df432691d49f1e4aed9e811be465b40
Ahmed, Daiyaan <daiyaan.ahmed@intel.com>
2023-08-22 14:41:34 +0800

    [SYCLomatic #1160] Test for path to helper function dir
    Signed-off-by: Ahmed, Daiyaan <daiyaan.ahmed@intel.com>

---
+++ behavior_tests/src/bt-helper-function-dir/do_test.py
@@ -0,0 +1,31 @@
+# ====------ do_test.py---------- *- Python -* ----===##
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+#
+# ===----------------------------------------------------------------------===#
+import subprocess
+import platform
+import os
+import sys
+
+from test_utils import *
+
+def setup_test():
+    change_dir(test_config.current_test)
+    return True
+
+def migrate_test():
+
+    call_subprocess(test_config.CT_TOOL +" --helper-function-dir")
+    helper_function_dir_root = os.path.abspath(os.path.join(get_ct_path(), os.pardir , os.pardir , "include"))
+    print("Helper functions directory: ", helper_function_dir_root)
+    return is_sub_string(helper_function_dir_root, test_config.command_output)
+    
+def build_test():
+    return True
+
+def run_test():
+    return True
