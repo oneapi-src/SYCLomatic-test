10dbafd9504e7674f71547b01ab95d8c6be0087a
Tang, Jiajun <jiajun.tang@intel.com>
2023-08-29 17:57:15 +0800

    [SYCLomatic #997] Fix comment.
    Signed-off-by: Tang, Jiajun jiajun.tang@intel.com

--- behavior_tests/src/helper_function_preference_no_queue_device/do_test.py
+++ behavior_tests/src/helper_function_preference_no_queue_device/do_test.py
@@ -1,4 +1,4 @@
-# ===---------- do_test.py---------- *- Python -* -------------------------===##
+# ===---------- do_test.py ---------- *- Python -* -------------------------===#
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
