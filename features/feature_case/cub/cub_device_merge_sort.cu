e0801deb843c4e2c9f34d5c08a45d17c781908f1
Wang, Yihan <yihan.wang@intel.com>
2023-10-30 10:27:37 +0800

    address comment

--- features/feature_case/cub/cub_device_merge_sort.cu
+++ features/feature_case/cub/cub_device_merge_sort.cu
@@ -1,4 +1,4 @@
-// ====------ cub_device_merge_sort.cu --------------------- *- CUDA -* ---===//
+// ===------ cub_device_merge_sort.cu --------------------- *- CUDA -* ---===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
