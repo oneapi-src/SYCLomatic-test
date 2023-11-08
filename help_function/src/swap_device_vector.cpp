e3aa0e1c82f650e94ce3b7cee4dce98f57acaeb9
Dan Hoeflinger <dan.hoeflinger@intel.com>
2023-11-08 13:24:47 -0500

    adding copyright notice
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- help_function/src/swap_device_vector.cpp
+++ help_function/src/swap_device_vector.cpp
@@ -1,3 +1,12 @@
+// ====------ swap_device_vector.cpp---------- -*- C++ -* ----===////
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//
+// ===----------------------------------------------------------------------===//
+
 #include <oneapi/dpl/execution>
 #include <oneapi/dpl/algorithm>
 #include <sycl.hpp>
