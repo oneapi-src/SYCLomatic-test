58974bac7afcad0bcddfc27fcfcb46dc67bd9502
Zhiwei Jiang <zhiwei.jiang@intel.com>
2023-11-20 08:19:00 +0800

    Update comments
    Signed-off-by: Zhiwei Jiang <zhiwei.jiang@intel.com>

--- help_function/src/sparse_utils_4_buffer.cpp
+++ help_function/src/sparse_utils_4_buffer.cpp
@@ -1,4 +1,4 @@
-// ===------- sparse_utils_3_buffer.cpp --------------------- *- C++ -* ----===//
+// ===------- sparse_utils_4_buffer.cpp --------------------- *- C++ -* ----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- help_function/src/sparse_utils_4_usm.cpp
+++ help_function/src/sparse_utils_4_usm.cpp
@@ -1,4 +1,4 @@
-// ===------- sparse_utils_3_usm.cpp ------------------------ *- C++ -* ----===//
+// ===------- sparse_utils_4_usm.cpp ------------------------ *- C++ -* ----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
