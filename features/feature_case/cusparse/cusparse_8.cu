1c4d896d73071071f495f6bb73abdf2fe82da40f
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2023-11-30 13:55:21 +0800

    Add more test
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- features/feature_case/cusparse/cusparse_8.cu
+++ features/feature_case/cusparse/cusparse_8.cu
@@ -106,7 +106,7 @@ bool test_passed = true;
 // | 1 1 2 0 |
 // | 0 1 3 0 |
 // | 0 0 1 5 |
-void test_cusparseCsr2csc() {
+void test_cusparseCsr2csc_00() {
   std::vector<float> a_val_vec = {1, 1, 2, 1, 3, 1, 5};
   Data<float> a_s_val(a_val_vec.data(), 7);
   Data<double> a_d_val(a_val_vec.data(), 7);
@@ -212,15 +212,132 @@ void test_cusparseCsr2csc() {
       compare_result(expect_b_row_ind, b_d_row_ind.h_data, 7) &&
       compare_result(expect_b_row_ind, b_c_row_ind.h_data, 7) &&
       compare_result(expect_b_row_ind, b_z_row_ind.h_data, 7))
-    printf("Csr2csc pass\n");
+    printf("Csr2csc 00 pass\n");
   else {
-    printf("Csr2csc fail\n");
+    printf("Csr2csc 00 fail\n");
+    test_passed = false;
+  }
+}
+
+// | 1 1 2 0 |
+// | 0 1 3 0 |
+// | 0 0 1 5 |
+void test_cusparseCsr2csc_01() {
+  std::vector<float> a_val_vec = {1, 1, 2, 1, 3, 1, 5};
+  Data<float> a_s_val(a_val_vec.data(), 7);
+  Data<double> a_d_val(a_val_vec.data(), 7);
+  Data<float2> a_c_val(a_val_vec.data(), 7);
+  Data<double2> a_z_val(a_val_vec.data(), 7);
+  std::vector<float> a_row_ptr_vec = {0, 3, 5, 7};
+  Data<int> a_s_row_ptr(a_row_ptr_vec.data(), 4);
+  Data<int> a_d_row_ptr(a_row_ptr_vec.data(), 4);
+  Data<int> a_c_row_ptr(a_row_ptr_vec.data(), 4);
+  Data<int> a_z_row_ptr(a_row_ptr_vec.data(), 4);
+  std::vector<float> a_col_ind_vec = {0, 1, 2, 1, 2, 2, 3};
+  Data<int> a_s_col_ind(a_col_ind_vec.data(), 7);
+  Data<int> a_d_col_ind(a_col_ind_vec.data(), 7);
+  Data<int> a_c_col_ind(a_col_ind_vec.data(), 7);
+  Data<int> a_z_col_ind(a_col_ind_vec.data(), 7);
+
+  Data<float> b_s_val(a_val_vec.data(), 7);
+  Data<double> b_d_val(a_val_vec.data(), 7);
+  Data<float2> b_c_val(a_val_vec.data(), 7);
+  Data<double2> b_z_val(a_val_vec.data(), 7);
+  Data<int> b_s_col_ptr(a_row_ptr_vec.data(), 5);
+  Data<int> b_d_col_ptr(a_row_ptr_vec.data(), 5);
+  Data<int> b_c_col_ptr(a_row_ptr_vec.data(), 5);
+  Data<int> b_z_col_ptr(a_row_ptr_vec.data(), 5);
+  Data<int> b_s_row_ind(a_col_ind_vec.data(), 7);
+  Data<int> b_d_row_ind(a_col_ind_vec.data(), 7);
+  Data<int> b_c_row_ind(a_col_ind_vec.data(), 7);
+  Data<int> b_z_row_ind(a_col_ind_vec.data(), 7);
+
+  cusparseHandle_t handle;
+  cusparseCreate(&handle);
+
+  a_s_val.H2D();
+  a_d_val.H2D();
+  a_c_val.H2D();
+  a_z_val.H2D();
+  a_s_row_ptr.H2D();
+  a_d_row_ptr.H2D();
+  a_c_row_ptr.H2D();
+  a_z_row_ptr.H2D();
+  a_s_col_ind.H2D();
+  a_d_col_ind.H2D();
+  a_c_col_ind.H2D();
+  a_z_col_ind.H2D();
+
+
+  size_t ws_size_s = 0;
+  size_t ws_size_d = 0;
+  size_t ws_size_c = 0;
+  size_t ws_size_z = 0;
+  cusparseCsr2cscEx2_bufferSize(handle, 3, 4, 7, a_s_val.d_data, a_s_row_ptr.d_data, a_s_col_ind.d_data, b_s_val.d_data, b_s_col_ptr.d_data, b_s_row_ind.d_data, CUDA_R_32F, CUSPARSE_ACTION_SYMBOLIC, CUSPARSE_INDEX_BASE_ZERO, CUSPARSE_CSR2CSC_ALG1, &ws_size_s);
+  cusparseCsr2cscEx2_bufferSize(handle, 3, 4, 7, a_d_val.d_data, a_d_row_ptr.d_data, a_d_col_ind.d_data, b_d_val.d_data, b_d_col_ptr.d_data, b_d_row_ind.d_data, CUDA_R_64F, CUSPARSE_ACTION_SYMBOLIC, CUSPARSE_INDEX_BASE_ZERO, CUSPARSE_CSR2CSC_ALG1, &ws_size_d);
+  cusparseCsr2cscEx2_bufferSize(handle, 3, 4, 7, a_c_val.d_data, a_c_row_ptr.d_data, a_c_col_ind.d_data, b_c_val.d_data, b_c_col_ptr.d_data, b_c_row_ind.d_data, CUDA_C_32F, CUSPARSE_ACTION_SYMBOLIC, CUSPARSE_INDEX_BASE_ZERO, CUSPARSE_CSR2CSC_ALG1, &ws_size_c);
+  cusparseCsr2cscEx2_bufferSize(handle, 3, 4, 7, a_z_val.d_data, a_z_row_ptr.d_data, a_z_col_ind.d_data, b_z_val.d_data, b_z_col_ptr.d_data, b_z_row_ind.d_data, CUDA_C_64F, CUSPARSE_ACTION_SYMBOLIC, CUSPARSE_INDEX_BASE_ZERO, CUSPARSE_CSR2CSC_ALG1, &ws_size_z);
+
+  void *ws_s;
+  void *ws_d;
+  void *ws_c;
+  void *ws_z;
+  cudaMalloc(&ws_s, ws_size_s);
+  cudaMalloc(&ws_d, ws_size_d);
+  cudaMalloc(&ws_c, ws_size_c);
+  cudaMalloc(&ws_z, ws_size_z);
+
+  cusparseCsr2cscEx2(handle, 3, 4, 7, a_s_val.d_data, a_s_row_ptr.d_data, a_s_col_ind.d_data, b_s_val.d_data, b_s_col_ptr.d_data, b_s_row_ind.d_data, CUDA_R_32F, CUSPARSE_ACTION_SYMBOLIC, CUSPARSE_INDEX_BASE_ZERO, CUSPARSE_CSR2CSC_ALG1, ws_s);
+  cusparseCsr2cscEx2(handle, 3, 4, 7, a_d_val.d_data, a_d_row_ptr.d_data, a_d_col_ind.d_data, b_d_val.d_data, b_d_col_ptr.d_data, b_d_row_ind.d_data, CUDA_R_64F, CUSPARSE_ACTION_SYMBOLIC, CUSPARSE_INDEX_BASE_ZERO, CUSPARSE_CSR2CSC_ALG1, ws_d);
+  cusparseCsr2cscEx2(handle, 3, 4, 7, a_c_val.d_data, a_c_row_ptr.d_data, a_c_col_ind.d_data, b_c_val.d_data, b_c_col_ptr.d_data, b_c_row_ind.d_data, CUDA_C_32F, CUSPARSE_ACTION_SYMBOLIC, CUSPARSE_INDEX_BASE_ZERO, CUSPARSE_CSR2CSC_ALG1, ws_c);
+  cusparseCsr2cscEx2(handle, 3, 4, 7, a_z_val.d_data, a_z_row_ptr.d_data, a_z_col_ind.d_data, b_z_val.d_data, b_z_col_ptr.d_data, b_z_row_ind.d_data, CUDA_C_64F, CUSPARSE_ACTION_SYMBOLIC, CUSPARSE_INDEX_BASE_ZERO, CUSPARSE_CSR2CSC_ALG1, ws_z);
+
+  b_s_val.D2H();
+  b_d_val.D2H();
+  b_c_val.D2H();
+  b_z_val.D2H();
+  b_s_col_ptr.D2H();
+  b_d_col_ptr.D2H();
+  b_c_col_ptr.D2H();
+  b_z_col_ptr.D2H();
+  b_s_row_ind.D2H();
+  b_d_row_ind.D2H();
+  b_c_row_ind.D2H();
+  b_z_row_ind.D2H();
+
+  cudaStreamSynchronize(0);
+
+  cudaFree(ws_s);
+  cudaFree(ws_d);
+  cudaFree(ws_c);
+  cudaFree(ws_z);
+  cusparseDestroy(handle);
+
+  float expect_b_val[7] = {0, 0, 0, 0, 0, 0, 0};
+  float expect_b_col_ptr[5] = {0, 1, 3, 6, 7};
+  float expect_b_row_ind[7] = {0, 0, 1, 0, 1, 2, 2};
+  if (compare_result(expect_b_val, b_s_val.h_data, 7) &&
+      compare_result(expect_b_val, b_d_val.h_data, 7) &&
+      compare_result(expect_b_val, b_c_val.h_data, 7) &&
+      compare_result(expect_b_val, b_z_val.h_data, 7) &&
+      compare_result(expect_b_col_ptr, b_s_col_ptr.h_data, 5) &&
+      compare_result(expect_b_col_ptr, b_d_col_ptr.h_data, 5) &&
+      compare_result(expect_b_col_ptr, b_c_col_ptr.h_data, 5) &&
+      compare_result(expect_b_col_ptr, b_z_col_ptr.h_data, 5) &&
+      compare_result(expect_b_row_ind, b_s_row_ind.h_data, 7) &&
+      compare_result(expect_b_row_ind, b_d_row_ind.h_data, 7) &&
+      compare_result(expect_b_row_ind, b_c_row_ind.h_data, 7) &&
+      compare_result(expect_b_row_ind, b_z_row_ind.h_data, 7))
+    printf("Csr2csc 01 pass\n");
+  else {
+    printf("Csr2csc 01 fail\n");
     test_passed = false;
   }
 }
 
 int main() {
-  test_cusparseCsr2csc();
+  test_cusparseCsr2csc_00();
+  test_cusparseCsr2csc_01();
 
   if (test_passed)
     return 0;
