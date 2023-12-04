3239858aae446a50d7b403ea6027882576d06dff
Chen, Sheng S <sheng.s.chen@intel.com>
2023-12-04 21:49:08 +0800

    [SYCLomatic] Add bitcracker test case.
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

---
+++ third-party-programs/Velocity-Bench/bitcracker/CUDA/build/CMakeFiles/bitcracker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bitcracker.dir/src/bitcracker_generated_attack.cu.o"
+  "CMakeFiles/bitcracker.dir/src/bitcracker_generated_main.cu.o"
+  "CMakeFiles/bitcracker.dir/src/bitcracker_generated_utils.cu.o"
+  "CMakeFiles/bitcracker.dir/src/bitcracker_generated_w_blocks.cu.o"
+  "bitcracker"
+  "bitcracker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/bitcracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
