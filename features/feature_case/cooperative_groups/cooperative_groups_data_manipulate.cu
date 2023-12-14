274c4c42ffc285cf08c7005c490fa5feb5caed0d
Chen, Sheng S <sheng.s.chen@intel.com>
2023-12-14 17:35:37 +0800

    fix test

--- features/feature_case/cooperative_groups/cooperative_groups_data_manipulate.cu
+++ features/feature_case/cooperative_groups/cooperative_groups_data_manipulate.cu
@@ -1,5 +1,4 @@
-// ====------ cooperative_groups_data_manipulate.cu --------------------- *-
-// CUDA -* ----===////
+// ====------ cooperative_groups_data_manipulate.cu --------------------- *- CUDA -* ----===////
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
