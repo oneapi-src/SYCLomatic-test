b2587d251b4a3385b10f950d4222d9f8874b3a76
Wang, Yihan <yihan.wang@intel.com>
2023-12-28 14:56:40 +0800

    [SYCLomatic #1465] Add test to check the rewording of DPCT1026
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

---
+++ behavior_tests/src/bt-check-warning1026/test.cu
@@ -0,0 +1,14 @@
+// ===------- test.cu ------------------------------------- *- CUDA -* ----===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//
+// ===---------------------------------------------------------------------===//
+
+#include <cuda.h>
+
+void test_1() {
+  cuInit(0);
+}
