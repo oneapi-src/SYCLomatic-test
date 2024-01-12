f06848b094fc6cdf0446e8aedae836db5a9a154c
Wang, Hao3 <hao3.wang@intel.com>
2024-01-12 22:15:59 +0800

    fix
    Signed-off-by: Wang, Hao3 <hao3.wang@intel.com>

--- features/test_feature.py
+++ features/test_feature.py
@@ -113,8 +113,8 @@ def migrate_test():
         with open(os.path.join(db_dir, 'compile_commands.json'), "r") as file:
             file_contents = file.read()
         file_contents = file_contents.replace(original_string, new_string)
-        db_dir_escaped = db_dir.replace('\\', '\\\\')
-        file_contents = re.sub(r'"directory": ".*?"', '"directory": "' + db_dir_escaped + '"', file_contents)
+        file_contents = re.sub(r'"directory": ".*?"', '"directory": "' + db_dir + '"', file_contents)
+        file_contents = file_contents.replace('\\', '\\\\')
         with open(os.path.join(db_dir, 'compile_commands.json'), "w") as file:
             file.write(file_contents)
         src.append(' -p=' + os.path.join(in_root, 'gpubuild'))
