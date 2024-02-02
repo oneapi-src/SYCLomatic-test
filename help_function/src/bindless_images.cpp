51097a91b24ccd8d4b476f4d16f988041f7d4ec5
Tang Jiajun <jiajun.tang@intel.com>
2024-02-02 17:06:31 +0800

    [SYCLomatic #1588] Fix bindless_images help_function test bug. (#608)
    Signed-off-by: Tang, Jiajun jiajun.tang@intel.com

--- help_function/src/bindless_images.cpp
+++ help_function/src/bindless_images.cpp
@@ -9,62 +9,66 @@
 
 #include <dpct/dpct.hpp>
 
-dpct::experimental::bindless_image_wrapper<sycl::float4, 4> w;
+dpct::experimental::bindless_image_wrapper<sycl::float4, 4> i;
 
 int main() {
   auto q = dpct::get_default_queue();
-  sycl::ext::oneapi::experimental::image_descriptor d(
-      {1, 1}, sycl::image_channel_order::rgba, sycl::image_channel_type::fp32);
-  dpct::experimental::image_mem_ptr mem =
-      new sycl::ext::oneapi::experimental::image_mem(d, q);
-  sycl::float4 *data =
-      (sycl::float4 *)dpct::dpct_malloc(sizeof(sycl::float4), q);
-  dpct::image_data img(mem);
-  img.set_data(mem);
-  size_t pitch;
-  sycl::float4 *data2d =
-      (sycl::float4 *)dpct::dpct_malloc(pitch, sizeof(sycl::float4), 1);
+  dpct::image_channel c = dpct::image_channel::create<sycl::float4>();
   dpct::sampling_info samp;
-  dpct::image_channel chan = dpct::image_channel::create<sycl::float4>();
+  auto f = (sycl::float4 *)dpct::dpct_malloc(sizeof(sycl::float4), q);
+  size_t p, x = 1, y = 1, z = 1;
+  auto f2d = (sycl::float4 *)dpct::dpct_malloc(p, sizeof(sycl::float4), 1);
   printf("prepare variable pass!\n");
-  dpct::experimental::async_dpct_memcpy(mem, 0, 0, data, 1, 1, 1, q);
-  dpct::experimental::dpct_memcpy(mem, 0, 0, data, 1, 1, 1, q);
-  dpct::experimental::async_dpct_memcpy(mem, 0, 0, data, 1, q);
-  dpct::experimental::dpct_memcpy(mem, 0, 0, data, 1, q);
-  dpct::experimental::async_dpct_memcpy(data, mem, 0, 0, 1, 1, 1, q);
-  dpct::experimental::dpct_memcpy(data, mem, 0, 0, 1, 1, 1, q);
-  dpct::experimental::async_dpct_memcpy(data, mem, 0, 0, 1, q);
-  dpct::experimental::dpct_memcpy(data, mem, 0, 0, 1, q);
-  dpct::experimental::dpct_memcpy(mem, 0, 0, mem, 0, 0, 1, 1, q);
-  dpct::experimental::dpct_memcpy(mem, 0, 0, mem, 0, 0, 1, q);
+  dpct::experimental::image_mem_wrapper_ptr m;
+  m = new dpct::experimental::image_mem_wrapper(c, sycl::range(1));
+  m = new dpct::experimental::image_mem_wrapper(c, x);
+  m = new dpct::experimental::image_mem_wrapper(c, x, y);
+  m = new dpct::experimental::image_mem_wrapper(c, x, y, z);
+  m->get_channel();
+  m->get_range();
+  m->get_desc();
+  m->get_handle();
+  dpct::image_data d; //(m); // TODO: need support.
+  d.set_data(m);
+  printf("image mem wrapper pass!\n");
+  dpct::experimental::async_dpct_memcpy(m, 0, 0, f, 1, 1, 1, q);
+  dpct::experimental::dpct_memcpy(m, 0, 0, f, 1, 1, 1, q);
+  dpct::experimental::async_dpct_memcpy(m, 0, 0, f, 1, q);
+  dpct::experimental::dpct_memcpy(m, 0, 0, f, 1, q);
+  dpct::experimental::async_dpct_memcpy(f, m, 0, 0, 1, 1, 1, q);
+  dpct::experimental::dpct_memcpy(f, m, 0, 0, 1, 1, 1, q);
+  dpct::experimental::async_dpct_memcpy(f, m, 0, 0, 1, q);
+  dpct::experimental::dpct_memcpy(f, m, 0, 0, 1, q);
+  dpct::experimental::dpct_memcpy(m, 0, 0, m, 0, 0, 1, 1, q);
+  dpct::experimental::dpct_memcpy(m, 0, 0, m, 0, 0, 1, q);
   printf("memory copy pass!\n");
-  auto h = dpct::experimental::create_bindless_image(img, samp, q);
+  auto h = dpct::experimental::create_bindless_image(d, samp, q);
   dpct::experimental::get_data(h);
   dpct::experimental::get_sampling_info(h);
-  dpct::experimental::get_channel(mem);
   dpct::experimental::destroy_bindless_image(h, q);
   printf("texture object pass!\n");
-  w.attach(data, 1, chan, q);
-  w.attach(data, 1, q);
-  w.attach(data2d, 1, 1, pitch, chan, q);
-  w.attach(data2d, 1, 1, pitch, chan, q);
-  w.attach(mem, chan, q);
-  w.attach(mem, q);
-  w.detach(q);
-  w.set_channel(chan);
-  w.get_channel();
-  w.set_channel_size(4, 1);
-  w.get_channel_size();
-  w.set_channel_data_type(dpct::image_channel_data_type::fp);
-  w.get_channel_data_type();
-  w.set(sycl::addressing_mode::repeat);
-  w.get_addressing_mode();
-  w.set(sycl::coordinate_normalization_mode::unnormalized);
-  w.is_coordinate_normalized();
-  w.set(sycl::filtering_mode::nearest);
-  w.get_filtering_mode();
-  w.get_handle();
+  i.attach(f, 1, c, q);
+  i.attach(f, 1, q);
+  i.attach(f2d, 1, 1, p, c, q);
+  i.attach(f2d, 1, 1, p, c, q);
+  i.attach(m, c, q);
+  i.attach(m, q);
+  i.detach(q);
+  i.set_channel(c);
+  i.get_channel();
+  i.set_channel_size(4, 1);
+  i.get_channel_size();
+  i.set_channel_data_type(dpct::image_channel_data_type::fp);
+  i.get_channel_data_type();
+  i.set(sycl::addressing_mode::repeat);
+  i.get_addressing_mode();
+  i.set(sycl::coordinate_normalization_mode::unnormalized);
+  i.is_coordinate_normalized();
+  i.set(sycl::filtering_mode::nearest);
+  i.get_filtering_mode();
+  i.get_handle();
   printf("texture referece pass!\n");
+  delete m;
   printf("test pass!\n");
   return 0;
 }
