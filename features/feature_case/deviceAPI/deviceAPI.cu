035b6366497833820b9d7e7e7b8f5f947593e5f0
Jiang, Zhiwei <zhiwei.jiang@intel.com>
2024-03-08 11:51:37 +0800

    [SYCLomatic #1752] Add test for migrating cudaDevAttrTextureAlignment
    Signed-off-by: Jiang, Zhiwei <zhiwei.jiang@intel.com>

--- features/feature_case/deviceAPI/deviceAPI.cu
+++ features/feature_case/deviceAPI/deviceAPI.cu
@@ -1,21 +1,22 @@
-// ====------ deviceAPI.cu---------- *- CUDA -* ----===////
+// ===------- deviceAPI.cu ------------------------------- *- CUDA -* ----=== //
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//
-// ===----------------------------------------------------------------------===//
+// ===--------------------------------------------------------------------=== //
 
 int main() {
   cudaSetDeviceFlags(cudaDeviceMapHost);
   float *d_dst;
   cudaStream_t stream;
-  //unsupported API
-  //cudaMemcpyPeerAsync(d_dst, 1, d_dst, 1, 111, stream);
+  // unsupported API
+  // cudaMemcpyPeerAsync(d_dst, 1, d_dst, 1, 111, stream);
 
   int concurrentManagedAccess = 0;
   int p_gpuDevice = 0;
-  int error = cudaDeviceGetAttribute(&concurrentManagedAccess,  cudaDevAttrConcurrentManagedAccess,  p_gpuDevice);
+  cudaDeviceGetAttribute(&concurrentManagedAccess, cudaDevAttrConcurrentManagedAccess, p_gpuDevice);
+  int alignment;
+  cudaDeviceGetAttribute(&alignment, cudaDevAttrTextureAlignment, p_gpuDevice);
   return 0;
 }
