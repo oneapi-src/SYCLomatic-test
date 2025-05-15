a8caa723accccec309dd59bcf78c5571de864106
Chen, Sheng S <sheng.s.chen@intel.com>
2025-05-14 23:51:26 -0700

    up
    Signed-off-by: Chen, Sheng S <sheng.s.chen@intel.com>

--- features/feature_case/deviceAPI/pointer_attributes.cu
+++ features/feature_case/deviceAPI/pointer_attributes.cu
@@ -1,3 +1,11 @@
+// ====------ pointer_attributes.cu---------- *- CUDA -* ----===////
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//
+// ===----------------------------------------------------------------------===//
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <stdlib.h>
