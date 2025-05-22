934ff7ddacc43292b18dde294788b987cb46f481
Teja Alaghari <teja.alaghari@intel.com>
2025-05-22 12:55:45 +0800

    Modified testing config

--- features/test_feature.py
+++ features/test_feature.py
@@ -249,6 +249,15 @@ def build_test():
             link_opts.append(' dnnl.lib')
     ret = False
 
+    if test_config.current_test == 'nvshmem':
+        ISHMEMROOT = os.environ['ISHMEMROOT']
+        ISHMEMVER = os.environ['ISHMEMVER']
+
+        if (ISHMEMROOT and ISHMEMVER):
+            link_opts.append(os.path.join(ISHMEMROOT, ISHMEMVER, 'lib', 'libishmem.a'))
+
+        link_opts.append('-lze_loader -lmpi')
+
     if (test_config.current_test == 'cufft-external-workspace'):
         manual_fix_for_cufft_external_workspace(srcs[0])
     if (test_config.current_test in occupancy_calculation_exper):
